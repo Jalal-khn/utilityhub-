2da4bb1692278d28c0b4d8473872ef81e49b4b27
jalal-khn <jalalkhan8704243@gmail.com>
2026-09-11 15:21:49 -0700

    security: allow google analytics + adtrafficquality beacon images in img-src

--- next.config.js
+++ next.config.js
@@ -32,7 +32,7 @@ const securityHeaders = [
       "default-src 'self'",
       `script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://pagead2.googlesyndication.com https://*.googlesyndication.com https://*.googleadservices.com https://*.doubleclick.net https://*.gstatic.com https://*.google.com https://*.google https://*.adtrafficquality.google${isDev ? " 'unsafe-eval'" : ""}`,
       "style-src 'self' 'unsafe-inline'",
-      "img-src 'self' data: blob: https://*.google.com https://*.googleadservices.com https://*.doubleclick.net https://*.gstatic.com https://*.googlesyndication.com",
+      "img-src 'self' data: blob: https://*.google.com https://*.googleadservices.com https://*.doubleclick.net https://*.gstatic.com https://*.googlesyndication.com https://*.google https://*.adtrafficquality.google https://*.googletagmanager.com",
       "font-src 'self' data: https://fonts.gstatic.com",
       `connect-src 'self' https://www.google-analytics.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://pagead2.googlesyndication.com https://*.googlesyndication.com https://*.googleadservices.com https://*.doubleclick.net https://*.gstatic.com https://*.google.com https://*.google https://*.adtrafficquality.google`,
       "worker-src 'self' blob:",
